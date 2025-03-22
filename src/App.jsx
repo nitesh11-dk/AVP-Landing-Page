c9de48e591c5bb588eab285a2483e8929147968a
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-23 00:28:31 +0530

    Update package dependencies and integrate Locomotive Scroll for enhanced scrolling effects

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import Services from './components/Services';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery.jsx';
+import LocomotiveScroll from 'locomotive-scroll';
 import {ThemeProvider} from './components/theme-provider';
 
 const App = () => {
+  const loco = new LocomotiveScroll();
   return (
     <div className=" w-full mx-auto ">
        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
